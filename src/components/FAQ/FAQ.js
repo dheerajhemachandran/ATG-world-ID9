0f07de8aa5880bd9365c6463cbd591b557d59668
dheerajhemachandran <dheerajhemachandrn@gamil.com>
2022-09-16 20:16:35 +0530

    button animation

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -21,7 +21,7 @@ const FAQ = () => {
   <div class="accordion-item">
     <h2 class="accordion-header" id="flush-headingTwo">
       <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseTwo" aria-expanded="false" aria-controls="flush-collapseTwo">
-      How does the Venturebuddy valuation tool work? Accordion Item #2
+      How does the Venturebuddy valuation tool work?
       </button>
     </h2>
     <div id="flush-collapseTwo" class="accordion-collapse collapse" aria-labelledby="flush-headingTwo" data-bs-parent="#accordionFlushExample">
